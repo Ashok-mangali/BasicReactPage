8253ca9712a91f6b26de4080590622b2910e4429
ashok <aashok59238@gmail.com>
2023-11-11 16:03:26 +0530

    basicpage

---
+++ Logo.js
@@ -0,0 +1,18 @@
+const element = (
+    <div className="super-over-league-container">
+      <h1 className="heading">Food Items</h1>
+      <div className="teams-container">
+        <img
+          src="./Fish Food.jpg"
+          className="logo-image"
+        />
+        <img
+          src="./Dessurt food.jpg"
+          className="logo-image"
+        />
+      </div>
+    </div>
+  );
+  
+  ReactDOM.render(element, document.getElementById("root"));
+  
